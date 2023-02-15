4fb3cfbefd6d61a69a5d52f65d7a6c97c96e0156
tommys7 <tomas.hojgr23@gmail.com>
2023-02-15 17:58:42 +0100

    test

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,24 @@ $(document).ready(function(){
       prevArrow: $('.feedback-prevArrow'),
       nextArrow: $('.feedback-nextArrow')
 
-  });  
+    });
 
 
+    $('.offer-slider').slick({
+      
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2
+      
+    });
+    
+    $('.').slick({
+      
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+      
+    }); 
+
+  
   });
